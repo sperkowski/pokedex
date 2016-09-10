1740cc92eb789cfcdb151a650cfacafbf4c4a705
sperkowski <sperkowski@gmail.com>
2016-09-10 07:20:18 -0700

    Initial Commit

---
+++ pokedex-working/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-working
+//
+//  Created by Steven Perkowski on 9/10/16.
+//  Copyright © 2016 Steven Perkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
